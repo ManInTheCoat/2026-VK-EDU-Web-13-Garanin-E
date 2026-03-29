ed587ca0016f727ae36d04545024480880c9229a
ManInTheCoat <erofey15042006@gmail.com>
2026-03-30 02:42:33 +0300

    done: добавлена верстка страниц, базовые настройки Django и README

---
+++ niceanswer/niceanswer/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+  return render(request, 'index.html')
+
+def question(request):
+  return render(request, 'question.html')
+
+def ask(request):
+  return render(request, 'ask.html')
+
+def login(request):
+  return render(request, 'login.html')
+
+def signup(request):
+  return render(request, 'signup.html')
+
+def profile(request):
+  return render(request, 'profile.html')
