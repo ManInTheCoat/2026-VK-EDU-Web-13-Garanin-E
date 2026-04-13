859615f2f171ae257f6e1b13c3d39bb530a404f0
ManInTheCoat <erofey15042006@gmail.com>
2026-04-14 01:40:55 +0300

    fix: адаптировал отображение форм, добавил контекстный процессор для sidebar, разделил логику

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,5 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from .utils import paginate
 
 QUESTIONS = []
 for i in range(1, 30):
@@ -43,20 +43,3 @@ def question(request, question_id):
 def ask(request):
   """Форма создания вопроса"""
   return render(request, 'questions/ask.html')
-
-def paginate(objects_list, request, per_page=10):
-  paginator = Paginator(objects_list, per_page)
-  page_number = request.GET.get('page', 1)
-
-  try:
-    page = paginator.page(page_number)
-  except PageNotAnInteger:
-    page = paginator.page(1)
-  except EmptyPage:
-    page = paginator.page(paginator.num_pages)
-
-  page.custom_page_range = paginator.get_elided_page_range(
-    page.number, on_each_side=2, on_ends=1
-  )
-
-  return page
